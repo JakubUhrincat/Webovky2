8067e199a6ee9851a76e36bf55dbc876b5d67f34
JakubU7 <5d2uhrincat@gmail.com>
2022-02-24 19:25:50 +0100

    Formular s menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+enum MENU{USERS,BOOKS,BORROWINGS}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'menu';
+
+  menu=MENU;
+  actualMenu: MENU= MENU.USERS;
+
+  user={userId:"",userName:"",contact:""}
+  book={bookId:"",bookName:"",author:"",available:""}
+  borrowing={borrowingId:"",borrowingBook:"",borrowingUser:""}
+  users:any=[];
+  books:any=[];
+  borrowings:any=[];
+
+  /*public pridaj():{
+    this.users.push({userId: {this.user.userId},name:{this.user.userName},contact:{user.contact}});
+  }
+
+   */
+
+public openMenu(m:MENU){
+  this.actualMenu=m;
+}
+
 }
