a3fab7a09696b8d0c4849dcd38fa60b4254ef722
karinpetrova <petrova.kajka@gmail.com>
2022-02-27 16:49:49 +0100

    karin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,14 +20,16 @@ export class AppComponent {
   borrowings:any=[];
 
 
+
  public pridaj(): void {
    if (this.actualMenu == this.menu.USERS) {
      this.users.push({userId: this.user.userId, userName: this.user.userName, contact: this.user.contact});
-   }else if(this.actualMenu == this.menu.BOOKS){
+   }else if(this.actualMenu == this.menu.BOOKS && this.user.userId == this.book.bookId){
      this.books.push({bookId: this.book.bookId,bookName: this.book.bookName,author:this.book.author,available:this.book.available})
-   }else
+   }else if(this.user.userId == this.borrowing.borrowingId && this.book.bookName == this.borrowing.borrowingBook && this.user.userName == this.borrowing.borrowingUser)
      this.borrowings.push({borrowingId:this.borrowing.borrowingId,borrowingBook:this.borrowing.borrowingBook,borrowingUser: this.borrowing.borrowingUser});
-   }
+ }
+
 
 
 
