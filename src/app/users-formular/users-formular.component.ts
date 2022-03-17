420fb1457bf8e4a06374e43fd2d849542db4fed3
karinpetrova <petrova.kajka@gmail.com>
2022-03-17 22:40:58 +0100

    users

--- src/app/users-formular/users-formular.component.ts
+++ src/app/users-formular/users-formular.component.ts
@@ -1,25 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import {User} from "../models/user.model";
+import { Component, EventEmitter,Input,Output} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import {User} from "../../models/user.model";
+
 
 @Component({
   selector: 'app-users-formular',
   templateUrl: './users-formular.component.html',
   styleUrls: ['./users-formular.component.css']
-})
-export class UsersFormularComponent implements OnInit {
-  user: User = [{userName: 'w', contact: 'w', userId: 'w'}];
-  actualMenu: any;
-  menu: any;
 
-  public pridaj(): void {
-    if (this.actualMenu == this.menu.USERS) {
-      this.users.push({userId: this.user.userId, userName: this.user.userName, contact: this.user.contact});
+})
+export class UsersFormularComponent {
+
+  @Input()
+  set user(data: User) {
+    if (data) {
+      this.fillForm(data);
     }
   }
+
+  @Output()
+  addUser = new EventEmitter<User>();
+
+  @Output()
+  editUser = new EventEmitter<User>();
+
+  form: FormGroup;
+
   constructor() {
+    this.createForm();
   }
 
-  ngOnInit(): void {
+  private createForm(): void {
+    this.form = new FormGroup({
+      id: new FormControl(null),
+      username: new FormControl(null),
+      contact: new FormControl(null)
+    });
   }
 
+  private fillForm(user: User): void {
+    /*
+    treba zadat hodnoty pre vsetky formControls vo formGroup, ak nie, hodi error
+    this.form.setValue({
+      meno: osoba.meno,
+      priezvisko: osoba.priezvisko
+    });
+    */
+
+    /* netreba zadat vsetky hodnoty formControls vo formGroup
+    this.form.patchValue({
+      meno: osoba.meno,
+      priezvisko: osoba.priezvisko
+    });
+     */
+
+
+    this.form.controls.id.setValue(user.userId);
+    this.form.controls.username.setValue(user.userName);
+    this.form.controls.contact.setValue(user.contact);
+  }
+
+  public pridaj(): void {
+    this.addUser.emit({ id: Math.random().toString(), username: this.form.value.username, contact: this.form.value.contact});
+    this.form.reset();
+  }
+
+  public uprav(): void {
+    this.editUser.emit(this.form.value);
+    this.form.reset();
+  }
+
+  public zrus(): void {
+    this.user = undefined;
+    this.form.reset();
+  }
 }
--- src/app/users-zoznam/users-zoznam.component.ts
+++ src/app/users-zoznam/users-zoznam.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {User} from '../../models/user.model';
 
 @Component({
   selector: 'app-users-zoznam',
   templateUrl: './users-zoznam.component.html',
   styleUrls: ['./users-zoznam.component.css']
 })
-export class UsersZoznamComponent implements OnInit {
+export class UsersZoznamComponent {
 
+  @Input()
+  users: User[] = [];
 
+  @Output()
+  editUser: EventEmitter<User> = new EventEmitter<User>();
 
-  ngOnInit(): void {
+  @Output()
+  deleteUser: EventEmitter<User> = new EventEmitter<User>();
+
+  uprav(user: User): void {
+    this.editUser.emit(user);
   }
 
+  zmaz(user: User): void {
+    this.deleteUser.emit(user);
+  }
 }
