5fb311b48940d6ac090a73a8745d2f5521fe4821
karinpetrova <petrova.kajka@gmail.com>
2022-03-23 20:58:43 +0100

    +book

--- src/app/models/book.model.ts
+++ src/app/models/book.model.ts
@@ -11,3 +11,16 @@ export class Book{
     this.author = book.author;
   }
 }
+
+export class BookZoznam {
+  bookId?: string;
+  bookName: string;
+  author: string;
+
+
+  constructor(book: BookZoznam) {
+    this.bookId = book.bookId;
+    this.bookName = book.bookName;
+    this.author = book.author;
+  }
+}
